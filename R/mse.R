c5e3b1cfa9924fd40592bb92b57f4b83f7e93d7d
zdebruine <zacharydebruine@gmail.com>
2021-07-23 08:44:44 -0400

    Update mse.R

--- R/mse.R
+++ R/mse.R
@@ -1,6 +1,8 @@
 #' Mean Squared Error (MSE) loss of a factor model
 #'
 #' @description Mean squared error of factor models \eqn{W} and \eqn{H} given sparse matrix \eqn{A}
+#'
+#' Calculates the cross-product of \eqn{wh} or \eqn{wdh} (if \eqn{d} is specified), subtracts that from \eqn{A}, squares the result, and calculates the mean of all values. Parallelization is used across all available threads as determined by OpenMP.
 #' 
 #' @param A sparse matrix (of or coercible to \code{dgCMatrix}) of samples (columns) by features (rows)
 #' @param w dense matrix of class \code{matrix} with factors (columns) by features (rows)
@@ -9,7 +11,13 @@
 #' @param precision "float" or "double" precision
 #' @return mean squared error of the factorization
 #' @export
-#'
+#' @examples
+#' \dontrun{
+#'   data(moca7k)
+#'   model <- nmf(moca7k, k = 10)
+#'   loss <- mse(moca7k, model$w, model$d, model$h, precision = "double")
+#'   cat("loss of rank-10 factorization: ", loss)
+#' }
 mse <- function(A, w, d = NULL, h, precision = "float") {
   if (is.null(d)) d <- rep(1, ncol(w))
   if (ncol(w) != nrow(h)) stop("number of columns in w are not equal to number of rows in h!")
