4d3af5943db762375065bb07b96c4cbffb81409e
zdebruine <zacharydebruine@gmail.com>
2021-08-19 11:02:15 -0400

    Add files via upload

--- inst/include/RcppML/nmf2.hpp
+++ inst/include/RcppML/nmf2.hpp
@@ -20,7 +20,7 @@ wdhmodel c_nmf2_sparse(
     const bool diag,
     const std::vector<unsigned int> samples) {
   
-  Eigen::MatrixXd h(2, A.cols()), wb(2, A.rows());
+  Eigen::MatrixXd h(2, A.cols());
   Eigen::VectorXd d = Eigen::VectorXd::Ones(2);
   double tol_ = 1;
   unsigned int it;
@@ -36,8 +36,10 @@ wdhmodel c_nmf2_sparse(
       // compute right-hand side of linear system, "b", in-place in h
       double b0 = 0, b1 = 0;
       for (Rcpp::dgCMatrix::InnerIterator it(A, samples[i]); it; ++it){
-        b0 += it.value() * w(0, it.row());
-        b1 += it.value() * w(1, it.row());
+        const double val = it.value();
+        const unsigned int r = it.row();
+        b0 += val * w(0, r);
+        b1 += val * w(1, r);
       }
       
       // solve least squares
@@ -65,36 +67,39 @@ wdhmodel c_nmf2_sparse(
     }
     
     // reset diagonal and scale "h"
-    for (unsigned int i = 0; diag && i < 2; ++i) {
-      d[i] = h.row(i).sum() + 1e-15;
-      for (unsigned int j = 0; j < h.cols(); ++j) h(i, j) /= d(i);
+    if(diag){
+      #pragma omp simd
+      for (unsigned int i = 0; i < 2; ++i) {
+        d[i] = h.row(i).sum() + 1e-15;
+        for (unsigned int j = 0; j < h.cols(); ++j) h(i, j) /= d(i);
+      }
     }
     
     // update w
     a = h * h.transpose();
     denom = a(0, 0) * a(1, 1) - a(0, 1) * a(0, 1);
-    wb.setZero();
+    w.setZero();
     for (unsigned int i = 0; i < samples.size(); ++i) {
       // compute right-hand side of linear system, "b", in-place in h
-      for (Rcpp::dgCMatrix::InnerIterator it(A, samples[i]); it; ++it){
-        wb(0, it.row()) += it.value() * h(0, i);
-        wb(1, it.row()) += it.value() * h(1, i);
-      }
+      for (Rcpp::dgCMatrix::InnerIterator it(A, samples[i]); it; ++it)
+      #pragma omp simd
+        for(unsigned int j = 0; j < 2; ++j)
+          w(j, it.row()) += it.value() * h(j, i);
     }
     
     for (unsigned int i = 0; i < A.rows(); ++i) {
       // solve least squares
       if (nonneg) {
-        const double a01b1 = a(0, 1) * wb(1, i);
-        const double a11b0 = a(1, 1) * wb(0, i);
+        const double a01b1 = a(0, 1) * w(1, i);
+        const double a11b0 = a(1, 1) * w(0, i);
         if (a11b0 < a01b1) {
           w(0, i) = 0;
-          w(1, i) = wb(1, i) / a(1, 1);
+          w(1, i) /= a(1, 1);
         } else {
-          const double a01b0 = a(0, 1) * wb(0, i);
-          const double a00b1 = a(0, 0) * wb(1, i);
+          const double a01b0 = a(0, 1) * w(0, i);
+          const double a00b1 = a(0, 0) * w(1, i);
           if (a00b1 < a01b0) {
-            w(0, i) = wb(0, i) / a(0, 0);
+            w(0, i) /= a(0, 0);
             w(1, i) = 0;
           } else {
             w(0, i) = (a11b0 - a01b1) / denom;
@@ -102,19 +107,24 @@ wdhmodel c_nmf2_sparse(
           }
         }
       } else {
-        w(0, i) = (a(1, 1) * wb(0, i) - a(0, 1) * wb(1, i)) / denom;
-        w(1, i) = (a(0, 0) * wb(1, i) - a(0, 1) * wb(0, i)) / denom;
+        double b0 = w(0, i);
+        w(0, i) = (a(1, 1) * b0 - a(0, 1) * w(1, i)) / denom;
+        w(1, i) = (a(0, 0) * w(1, i) - a(0, 1) * b0) / denom;
       }
     }
     
     // reset diagonal and scale "w"
-    for (unsigned int i = 0; diag && i < 2; ++i) {
-      d[i] = w.row(i).sum() + 1e-15;
-      for (unsigned int j = 0; j < A.rows(); ++j) w(i, j) /= d(i);
+    if(diag){
+      #pragma omp simd
+      for (unsigned int i = 0; i < 2; ++i) {
+        d[i] = w.row(i).sum() + 1e-15;
+        for (unsigned int j = 0; j < A.rows(); ++j) w(i, j) /= d(i);
+      }
     }
-   
+    
     // calculate tolerance and check for convergence
     tol_ = cor(w, w_it);
+    Rcpp::checkUserInterrupt();
     if (verbose) Rprintf("%4d | %8.2e\n", it + 1, tol_);
     if (tol_ < tol) break;
   }
@@ -140,8 +150,8 @@ wdhmodel c_nmf2_dense(
     const bool verbose,
     const bool diag,
     const std::vector<unsigned int> samples) {
-  
-  Eigen::MatrixXd h(2, A.cols()), wb(2, A.rows());
+
+   Eigen::MatrixXd h(2, A.cols());
   Eigen::VectorXd d = Eigen::VectorXd::Ones(2);
   double tol_ = 1;
   unsigned int it;
@@ -157,8 +167,9 @@ wdhmodel c_nmf2_dense(
       // compute right-hand side of linear system, "b", in-place in h
       double b0 = 0, b1 = 0;
       for (unsigned int it = 0; it < A.rows(); ++it){
-        b0 += A(it, samples[i]) * w(0, it);
-        b1 += A(it, samples[i]) * w(1, it);
+        const double val = A(it, samples[i]);
+        b0 += val * w(0, it);
+        b1 += val * w(1, it);
       }
       
       // solve least squares
@@ -186,36 +197,39 @@ wdhmodel c_nmf2_dense(
     }
     
     // reset diagonal and scale "h"
-    for (unsigned int i = 0; diag && i < 2; ++i) {
-      d[i] = h.row(i).sum() + 1e-15;
-      for (unsigned int j = 0; j < h.cols(); ++j) h(i, j) /= d(i);
+    if(diag){
+      #pragma omp simd
+      for (unsigned int i = 0; i < 2; ++i) {
+        d[i] = h.row(i).sum() + 1e-15;
+        for (unsigned int j = 0; j < h.cols(); ++j) h(i, j) /= d(i);
+      }
     }
-
+    
     // update w
     a = h * h.transpose();
     denom = a(0, 0) * a(1, 1) - a(0, 1) * a(0, 1);
-    wb.setZero();
+    w.setZero();
     for (unsigned int i = 0; i < samples.size(); ++i) {
       // compute right-hand side of linear system, "b", in-place in h
-      for (unsigned int it = 0; it < A.rows(); ++it){
-        wb(0, it) += A(it, samples[i]) * h(0, i);
-        wb(1, it) += A(it, samples[i]) * h(1, i);
-      }
+      for (unsigned int it = 0; it < A.rows(); ++it)
+        #pragma omp simd
+        for(unsigned int j = 0; j < 2; ++j)
+          w(j, it) += A(it, samples[i]) * h(j, i);
     }
     
     for (unsigned int i = 0; i < A.rows(); ++i) {
       // solve least squares
       if (nonneg) {
-        const double a01b1 = a(0, 1) * wb(1, i);
-        const double a11b0 = a(1, 1) * wb(0, i);
+        const double a01b1 = a(0, 1) * w(1, i);
+        const double a11b0 = a(1, 1) * w(0, i);
         if (a11b0 < a01b1) {
           w(0, i) = 0;
-          w(1, i) = wb(1, i) / a(1, 1);
+          w(1, i) /= a(1, 1);
         } else {
-          const double a01b0 = a(0, 1) * wb(0, i);
-          const double a00b1 = a(0, 0) * wb(1, i);
+          const double a01b0 = a(0, 1) * w(0, i);
+          const double a00b1 = a(0, 0) * w(1, i);
           if (a00b1 < a01b0) {
-            w(0, i) = wb(0, i) / a(0, 0);
+            w(0, i) /= a(0, 0);
             w(1, i) = 0;
           } else {
             w(0, i) = (a11b0 - a01b1) / denom;
@@ -223,19 +237,24 @@ wdhmodel c_nmf2_dense(
           }
         }
       } else {
-        w(0, i) = (a(1, 1) * wb(0, i) - a(0, 1) * wb(1, i)) / denom;
-        w(1, i) = (a(0, 0) * wb(1, i) - a(0, 1) * wb(0, i)) / denom;
+        double b0 = w(0, i);
+        w(0, i) = (a(1, 1) * b0 - a(0, 1) * w(1, i)) / denom;
+        w(1, i) = (a(0, 0) * w(1, i) - a(0, 1) * b0) / denom;
       }
     }
     
     // reset diagonal and scale "w"
-    for (unsigned int i = 0; diag && i < 2; ++i) {
-      d[i] = w.row(i).sum() + 1e-15;
-      for (unsigned int j = 0; j < A.rows(); ++j) w(i, j) /= d(i);
+    if(diag){
+      #pragma omp simd
+      for (unsigned int i = 0; i < 2; ++i) {
+        d[i] = w.row(i).sum() + 1e-15;
+        for (unsigned int j = 0; j < A.rows(); ++j) w(i, j) /= d(i);
+      }
     }
-
+    
     // calculate tolerance and check for convergence
     tol_ = cor(w, w_it);
+    Rcpp::checkUserInterrupt();
     if (verbose) Rprintf("%4d | %8.2e\n", it + 1, tol_);
     if (tol_ < tol) break;
   }
@@ -248,7 +267,7 @@ wdhmodel c_nmf2_dense(
     d(1) = d(0); 
     d(0) = d1;
   }
-  
+    
   return wdhmodel{w.transpose(), d, h, tol_, it};
 }
 
