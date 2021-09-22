1e6c5fd7aae9d6eecfd5169e7230d038e1a0ae2b
zdebruine <zacharydebruine@gmail.com>
2021-09-22 16:38:49 -0400

    moved advanced params to ... for clarity

--- R/bipartition.R
+++ R/bipartition.R
@@ -10,9 +10,17 @@
 #'
 #' This function is a specialization of rank-2 \code{\link{nmf}} with support for factorization of only a subset of samples, and with additional calculations on the factorization model relevant to bipartitioning. See \code{\link{nmf}} for details regarding rank-2 factorization.
 #'
+#' @section Advanced Parameters:
+#' Several parameters may be specified in the \code{...} argument:
+#'
+#' * \code{diag = TRUE}: scale factors in \eqn{w} and \eqn{h} to sum to 1 by introducing a diagonal, \eqn{d}. This should generally never be set to \code{FALSE}. Diagonalization enables symmetry of models in factorization of symmetric matrices, convex L1 regularization, and consistent factor scalings.
+#' * \code{samples = 1:ncol(A)}: samples to include in bipartition, numbered from 1 to \code{ncol(A)}. Default is all samples.
+#' * \code{calc_dist = TRUE}: calculate the relative cosine distance of samples within a cluster to either cluster centroid. If \code{TRUE}, centers for clusters will also be calculated.
+#' * \code{seed = NULL}: random seed for model initialization, generally not needed for rank-2 factorizations because robust solutions are recovered when \code{diag = TRUE}
+#' * \code{maxit = 100}: maximum number of alternating updates of \eqn{w} and \eqn{h}. Generally, rank-2 factorizations converge quickly and this should not need to be adjusted.
+#'
 #' @inheritParams nmf
-#' @param samples samples to include in bipartition, numbered from 1 to \code{ncol(A)}. Default is \code{NULL} for all samples.
-#' @param calc_dist calculate the relative cosine distance of samples within a cluster to either cluster centroid. If \code{TRUE}, centers for clusters will also be calculated.
+#' @param nonneg enforce non-negativity of the rank-2 factorization used for bipartitioning
 #' @return
 #' A list giving the bipartition and useful statistics:
 #' 	\itemize{
@@ -42,7 +50,12 @@
 #' A <- as(as.matrix(iris[,1:4]), "dgCMatrix")
 #' bipartition(A, calc_dist = TRUE)
 #' }
-bipartition <- function(A, tol = 1e-5, maxit = 100, nonneg = TRUE, samples = 1:ncol(A), seed = NULL, verbose = FALSE, calc_dist = FALSE, diag = TRUE){
+bipartition <- function(A, tol = 1e-5, verbose = FALSE, nonneg = TRUE, ...){
+
+  p <- list(...)
+  defaults <- list("diag" = TRUE, "samples" = 1:ncol(A), "seed" = NULL, "calc_dist" = TRUE, "maxit" = 100)
+  for(i in 1:length(defaults))
+    if(is.null(p[[names(defaults)[[i]]]])) p[[names(defaults)[[i]]]] <- defaults[[i]]
 
   if (is(A, "sparseMatrix")) {
     A <- as(A, "dgCMatrix")
@@ -50,14 +63,13 @@ bipartition <- function(A, tol = 1e-5, maxit = 100, nonneg = TRUE, samples = 1:n
     A <- as.matrix(A)
   } else stop("'A' was not coercible to a matrix")
 
-    if(!is.numeric(seed)) seed <- 0
-    if(min(samples) == 0) stop("sample indices must be strictly positive")
-    if(max(samples) > ncol(A)) stop("sample indices must be strictly less than the number of columns in 'A'")
-    samples <- samples - 1
+    if(!is.numeric(p$seed)) p$seed <- 0
+    if(min(p$samples) == 0) stop("sample indices must be strictly positive")
+    if(max(p$samples) > ncol(A)) stop("sample indices must be strictly less than the number of columns in 'A'")
 
-    if(class(A) == "dgCMatrix"){
-        Rcpp_bipartition_sparse(A, tol, maxit, nonneg, samples, seed, verbose, calc_dist, diag)
+    if(class(A)[[1]] == "dgCMatrix"){
+        Rcpp_bipartition_sparse(A, tol, p$maxit, nonneg, p$samples - 1, p$seed, verbose, p$calc_dist, p$diag)
     } else {
-        Rcpp_bipartition_dense(A, tol, maxit, nonneg, samples, seed, verbose, calc_dist, diag)
+        Rcpp_bipartition_dense(A, tol, p$maxit, nonneg, p$samples - 1, p$seed, verbose, p$calc_dist, p$diag)
     }
 }
