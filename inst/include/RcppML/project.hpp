31518b1d680650f23fad03182e13405f737a4f77
zdebruine <zacharydebruine@gmail.com>
2021-09-22 16:45:37 -0400

    mask_zero support for projectInPlace (sparse), feature/sample support for all projections

--- inst/include/RcppML/project.hpp
+++ inst/include/RcppML/project.hpp
@@ -91,30 +91,52 @@ inline void nnls2InPlace(const Eigen::Matrix2d& a, const double denom, Eigen::Ma
 }
 
 void project(RcppML::SparseMatrix& A, const Eigen::MatrixXd& w, Eigen::MatrixXd& h, const bool nonneg, const double L1,
-             const unsigned int threads, const bool mask_zeros) {
+             const unsigned int threads, const bool mask_zeros, const std::vector<unsigned int>& samples, const std::vector<unsigned int>& features) {
 
+  bool all_features = (features.size() == A.rows());
   if (w.rows() == 1) {
     h.setZero();
     double a = 0;
     for (unsigned int i = 0; i < w.cols(); ++i) a += w(0, i) * w(0, i);
-    for (unsigned int i = 0; i < h.cols(); ++i) {
-      for (RcppML::SparseMatrix::InnerIterator it(A, i); it; ++it)
-        h(0, i) += it.value() * w(0, it.row());
-      h(0, i) /= a;
+    if (all_features) {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        for (RcppML::SparseMatrix::InnerIterator it(A, samples[i]); it; ++it)
+          h(0, i) += it.value() * w(0, it.row());
+        h(0, i) /= a;
+      }
+    } else {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        for (RcppML::SparseMatrix::InnerRangedIterator it(A, samples[i], features); it; ++it)
+          h(0, i) += it.value() * w(0, it.rangedRow());
+        h(0, i) /= a;
+      }
     }
   } else if (w.rows() == 2) {
     Eigen::Matrix2d a = w * w.transpose();
     a.diagonal().array() += TINY_NUM;
     const double denom = a(0, 0) * a(1, 1) - a(0, 1) * a(0, 1);
-    for (unsigned int i = 0; i < h.cols(); ++i) {
-      double b0 = 0, b1 = 0;
-      for (RcppML::SparseMatrix::InnerIterator it(A, i); it; ++it) {
-        const double val = it.value();
-        const unsigned int r = it.row();
-        b0 += val * w(0, r);
-        b1 += val * w(1, r);
+    if (all_features) {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        double b0 = 0, b1 = 0;
+        for (RcppML::SparseMatrix::InnerIterator it(A, samples[i]); it; ++it) {
+          const double val = it.value();
+          const unsigned int r = it.row();
+          b0 += val * w(0, r);
+          b1 += val * w(1, r);
+        }
+        nnls2(a, b0, b1, denom, h, i, nonneg);
       }
-      nnls2(a, b0, b1, denom, h, i, nonneg);
+    } else {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        double b0 = 0, b1 = 0;
+        for (RcppML::SparseMatrix::InnerRangedIterator it(A, samples[i], features); it; ++it) {
+          const double val = it.value();
+          const unsigned int r = it.rangedRow();
+          b0 += val * w(0, r);
+          b1 += val * w(1, r);
+        }
+        nnls2(a, b0, b1, denom, h, i, nonneg);
+      }
     }
   } else {
     if (!mask_zeros) {
@@ -126,8 +148,13 @@ void project(RcppML::SparseMatrix& A, const Eigen::MatrixXd& w, Eigen::MatrixXd&
       #endif
       for (unsigned int i = 0; i < h.cols(); ++i) {
         Eigen::VectorXd b = Eigen::VectorXd::Zero(h.rows());
-        for (RcppML::SparseMatrix::InnerIterator it(A, i); it; ++it)
-          b += it.value() * w.col(it.row());
+        if (all_features) {
+          for (RcppML::SparseMatrix::InnerIterator it(A, samples[i]); it; ++it)
+            b += it.value() * w.col(it.row());
+        } else {
+          for (RcppML::SparseMatrix::InnerRangedIterator it(A, samples[i], features); it; ++it)
+            b += it.value() * w.col(it.rangedRow());
+        }
         if (L1 != 0) b.array() -= L1;
 
         h.col(i) = a_llt.solve(b);
@@ -144,14 +171,19 @@ void project(RcppML::SparseMatrix& A, const Eigen::MatrixXd& w, Eigen::MatrixXd&
       #pragma omp parallel for num_threads(threads) schedule(dynamic)
       #endif
       for (unsigned int i = 0; i < h.cols(); ++i) {
-        Eigen::VectorXi nnz = Rcpp::as<Eigen::VectorXi>(A.nonzeroRows(i));
+        Eigen::VectorXi nnz = Rcpp::as<Eigen::VectorXi>(A.nonzeroRows(samples[i]));
         Eigen::MatrixXd w_ = submat(w, nnz);
         Eigen::MatrixXd a = w_ * w_.transpose();
         a.diagonal().array() += TINY_NUM;
 
         Eigen::VectorXd b = Eigen::VectorXd::Zero(h.rows());
-        for (RcppML::SparseMatrix::InnerIterator it(A, i); it; ++it)
-          b += it.value() * w.col(it.row());
+        if (all_features) {
+          for (RcppML::SparseMatrix::InnerIterator it(A, samples[i]); it; ++it)
+            b += it.value() * w.col(it.row());
+        } else {
+          for (RcppML::SparseMatrix::InnerRangedIterator it(A, samples[i], features); it; ++it)
+            b += it.value() * w.col(it.rangedRow());
+        }
         if (L1 != 0) b.array() -= L1;
 
         if (!nonneg) h.col(i) = a.llt().solve(b);
@@ -162,29 +194,50 @@ void project(RcppML::SparseMatrix& A, const Eigen::MatrixXd& w, Eigen::MatrixXd&
 }
 
 void project(const Eigen::MatrixXd& A, const Eigen::MatrixXd& w, Eigen::MatrixXd& h, const bool nonneg, const double L1,
-             const unsigned int threads, const bool mask_zeros) {
+             const unsigned int threads, const bool mask_zeros, const std::vector<unsigned int>& samples, const std::vector<unsigned int>& features) {
 
+  bool all_features = (features.size() == (unsigned int)A.rows());
   if (w.rows() == 1) {
     h.setZero();
     double a = 0;
     for (unsigned int i = 0; i < w.cols(); ++i) a += w(0, i) * w(0, i);
-    for (unsigned int i = 0; i < h.cols(); ++i) {
-      for (int j = 0; j < A.rows(); ++j)
-        h(0, i) += A(j, i) * w(0, j);
-      h(0, i) /= a;
+    if (all_features) {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        for (int j = 0; j < w.cols(); ++j)
+          h(0, i) += A(j, samples[i]) * w(0, j);
+        h(0, i) /= a;
+      }
+    } else {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        for (int j = 0; j < w.cols(); ++j)
+          h(0, i) += A(features[j], samples[i]) * w(0, j);
+        h(0, i) /= a;
+      }
     }
   } else if (w.rows() == 2) {
     Eigen::Matrix2d a = w * w.transpose();
     a.diagonal().array() += TINY_NUM;
     const double denom = a(0, 0) * a(1, 1) - a(0, 1) * a(0, 1);
-    for (unsigned int i = 0; i < h.cols(); ++i) {
-      double b0 = 0, b1 = 0;
-      for (int j = 0; j < A.rows(); ++j) {
-        const double val = A(j, i);
-        b0 += val * w(0, j);
-        b1 += val * w(1, j);
+    if (all_features) {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        double b0 = 0, b1 = 0;
+        for (int j = 0; j < w.cols(); ++j) {
+          const double val = A(j, samples[i]);
+          b0 += val * w(0, j);
+          b1 += val * w(1, j);
+        }
+        nnls2(a, b0, b1, denom, h, i, nonneg);
       }
-      nnls2(a, b0, b1, denom, h, i, nonneg);
+    } else {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        double b0 = 0, b1 = 0;
+        for (int j = 0; j < w.cols(); ++j) {
+          const double val = A(features[j], samples[i]);
+          b0 += val * w(0, j);
+          b1 += val * w(1, j);
+        }
+        nnls2(a, b0, b1, denom, h, i, nonneg);
+      }
     }
   } else {
     Eigen::MatrixXd a = w * w.transpose();
@@ -195,7 +248,12 @@ void project(const Eigen::MatrixXd& A, const Eigen::MatrixXd& w, Eigen::MatrixXd
     #endif
     for (unsigned int i = 0; i < h.cols(); ++i) {
       Eigen::VectorXd b = Eigen::VectorXd::Zero(a.rows());
-      b += w * A.col(i);
+      if (all_features) {
+        b += w * A.col(samples[i]);
+      } else {
+        for (unsigned int j = 0; j < w.cols(); ++j)
+          b += A(features[j], samples[i]) * w.col(j);
+      }
       if (L1 != 0) b.array() -= L1;
 
       h.col(i) = a_llt.solve(b);
@@ -209,77 +267,162 @@ void project(const Eigen::MatrixXd& A, const Eigen::MatrixXd& w, Eigen::MatrixXd
   }
 }
 
-void projectInPlace(RcppML::SparseMatrix& A, const Eigen::MatrixXd& h, Eigen::MatrixXd& w, const bool nonneg, const double L1,
-                    const unsigned int threads, const bool mask_zeros) {
+void project(RcppML::SparseMatrix& A, const Eigen::MatrixXd& w, Eigen::MatrixXd& h, const bool nonneg, const double L1,
+             const unsigned int threads, const bool mask_zeros) {
+  std::vector<unsigned int> samples(h.cols()), features(w.cols());
+  std::iota(samples.begin(), samples.end(), 0);
+  std::iota(features.begin(), features.end(), 0);
+  project(A, w, h, nonneg, L1, threads, mask_zeros, samples, features);
+}
 
+void project(const Eigen::MatrixXd& A, const Eigen::MatrixXd& w, Eigen::MatrixXd& h, const bool nonneg, const double L1,
+             const unsigned int threads, const bool mask_zeros) {
+  std::vector<unsigned int> samples(h.cols()), features(w.cols());
+  std::iota(samples.begin(), samples.end(), 0);
+  std::iota(features.begin(), features.end(), 0);
+  project(A, w, h, nonneg, L1, threads, mask_zeros, samples, features);
+}
+
+void projectInPlace(RcppML::SparseMatrix& A, const Eigen::MatrixXd& h, Eigen::MatrixXd& w, const bool nonneg, const double L1,
+                    const unsigned int threads, const bool mask_zeros, const std::vector<unsigned int>& samples, const std::vector<unsigned int>& features) {
+
+  bool all_features = (features.size() == A.rows());
   const unsigned int k = w.rows();
+  w.setZero();
   if (k == 1) {
-    w.setZero();
     double a = 0;
     for (unsigned int i = 0; i < h.cols(); ++i) a += h(0, i) * h(0, i);
-    for (unsigned int i = 0; i < h.cols(); ++i)
-      for (RcppML::SparseMatrix::InnerIterator it(A, i); it; ++it)
-        w(0, it.row()) += it.value() * h(0, i);
+    if (all_features) {
+      for (unsigned int i = 0; i < h.cols(); ++i)
+        for (RcppML::SparseMatrix::InnerIterator it(A, samples[i]); it; ++it)
+          w(0, it.row()) += it.value() * h(0, i);
+    } else {
+      for (unsigned int i = 0; i < h.cols(); ++i)
+        for (RcppML::SparseMatrix::InnerRangedIterator it(A, samples[i], features); it; ++it)
+          w(0, it.rangedRow()) += it.value() * h(0, i);
+    }
     for (unsigned int i = 0; i < w.cols(); ++i) w(0, i) /= a;
   } else if (k == 2) {
     Eigen::Matrix2d a = h * h.transpose();
     a.diagonal().array() += TINY_NUM;
     const double denom = a(0, 0) * a(1, 1) - a(0, 1) * a(0, 1);
-    w.setZero();
-    for (unsigned int i = 0; i < h.cols(); ++i) {
-      for (RcppML::SparseMatrix::InnerIterator it(A, i); it; ++it)
-        for (unsigned int j = 0; j < 2; ++j)
-          w(j, it.row()) += it.value() * h(j, i);
+    if (all_features) {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        for (RcppML::SparseMatrix::InnerIterator it(A, samples[i]); it; ++it)
+          for (unsigned int j = 0; j < 2; ++j)
+            w(j, it.row()) += it.value() * h(j, i);
+      }
+    } else {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        for (RcppML::SparseMatrix::InnerRangedIterator it(A, samples[i], features); it; ++it)
+          for (unsigned int j = 0; j < 2; ++j)
+            w(j, it.rangedRow()) += it.value() * h(j, i);
+      }
     }
     nnls2InPlace(a, denom, w, nonneg);
   } else {
-    Eigen::MatrixXd a = h * h.transpose();
-    a.diagonal().array() += TINY_NUM;
-    Eigen::LLT<Eigen::MatrixXd, 1> a_llt = a.llt();
+    // calculate "b", right-hand side of system of equations
     #ifdef _OPENMP
     #pragma omp parallel for num_threads(threads) schedule(dynamic)
     #endif
     for (unsigned int i = 0; i < h.cols(); ++i) {
-      for (RcppML::SparseMatrix::InnerIterator it(A, i); it; ++it)
-        for (unsigned int j = 0; j < k; ++j)
-          w(j, it.row()) += it.value() * h(j, i);
+      if (all_features) {
+        for (RcppML::SparseMatrix::InnerIterator it(A, samples[i]); it; ++it)
+          for (unsigned int j = 0; j < k; ++j)
+            w(j, it.row()) += it.value() * h(j, i);
+      } else {
+        for (RcppML::SparseMatrix::InnerRangedIterator it(A, samples[i], features); it; ++it)
+          for (unsigned int j = 0; j < k; ++j)
+            w(j, it.rangedRow()) += it.value() * h(j, i);
+      }
     }
     if (L1 != 0) w.array() -= L1;
-    #ifdef _OPENMP
-    #pragma omp parallel for num_threads(threads) schedule(dynamic)
-    #endif
-    for (unsigned int i = 0; i < w.cols(); ++i) {
-      Eigen::VectorXd b = w.col(i);
-      w.col(i) = a_llt.solve(b);
-      if (nonneg && (w.col(i).array() < 0).any()) {
-        b -= a * w.col(i);
-        c_nnls(a, b, w, i);
+
+    if (!mask_zeros) {
+      // calculate "a", left-hand side of system of equations
+      Eigen::MatrixXd a = h * h.transpose();
+      a.diagonal().array() += TINY_NUM;
+      Eigen::LLT<Eigen::MatrixXd, 1> a_llt = a.llt();
+
+      // solve systems 
+      #ifdef _OPENMP
+      #pragma omp parallel for num_threads(threads) schedule(dynamic)
+      #endif
+      for (unsigned int i = 0; i < w.cols(); ++i) {
+        Eigen::VectorXd b = w.col(i);
+        w.col(i) = a_llt.solve(b);
+        if (nonneg && (w.col(i).array() < 0).any()) {
+          b -= a * w.col(i);
+          c_nnls(a, b, w, i);
+        }
+      }
+    } else {
+      #ifdef _OPENMP
+      #pragma omp parallel for num_threads(threads) schedule(dynamic)
+      #endif
+      for (int i = 0; i < w.cols(); ++i) {
+        // calculate "a", left-hand side of system of equations
+
+        // get indices in samples for which A.row(i) is non-zero
+        std::vector<int> nnz_;
+        for (unsigned int col = 0; col < h.cols(); ++col) {
+          for (RcppML::SparseMatrix::InnerIterator it(A, samples[col]); it; ++it) {
+            if (it.row() > i) continue;
+            if (it.row() == i) {
+              nnz_.push_back(col);
+              continue;
+            }
+          }
+        }
+        Eigen::VectorXi nnz = Eigen::VectorXi::Map(nnz_.data(), nnz_.size());
+        Eigen::MatrixXd h_ = submat(h, nnz);
+        Eigen::MatrixXd a = h_ * h_.transpose();
+        a.diagonal().array() += TINY_NUM;
+
+        // solve systems
+        Eigen::VectorXd b = w.col(i);
+        if (!nonneg) w.col(i) = a.llt().solve(b);
+        else c_nnls(a, b, w, i);
       }
     }
   }
 }
 
 void projectInPlace(const Eigen::MatrixXd& A, const Eigen::MatrixXd& h, Eigen::MatrixXd& w, const bool nonneg, const double L1,
-                    const unsigned int threads, const bool mask_zeros) {
+                    const unsigned int threads, const bool mask_zeros, const std::vector<unsigned int>& samples, const std::vector<unsigned int>& features) {
 
+  bool all_features = (features.size() == (unsigned int)A.rows());
   const unsigned int k = w.rows();
+  w.setZero();
   if (k == 1) {
-    w.setZero();
     double a = 0;
     for (unsigned int i = 0; i < h.cols(); ++i) a += h(0, i) * h(0, i);
-    for (unsigned int i = 0; i < h.cols(); ++i)
-      for (int j = 0; j < A.rows(); ++j)
-        w(0, j) += A(j, i) * h(0, i);
+    if (all_features) {
+      for (unsigned int i = 0; i < h.cols(); ++i)
+        for (int j = 0; j < w.cols(); ++j)
+          w(0, j) += A(j, samples[i]) * h(0, i);
+    } else {
+      for (unsigned int i = 0; i < h.cols(); ++i)
+        for (int j = 0; j < w.cols(); ++j)
+          w(0, j) += A(features[j], samples[i]) * h(0, i);
+    }
     for (unsigned int i = 0; i < w.cols(); ++i) w(0, i) /= a;
   } else if (k == 2) {
     Eigen::Matrix2d a = h * h.transpose();
     a.diagonal().array() += TINY_NUM;
     const double denom = a(0, 0) * a(1, 1) - a(0, 1) * a(0, 1);
-    w.setZero();
-    for (unsigned int i = 0; i < h.cols(); ++i) {
-      for (int j = 0; j < A.rows(); ++j)
-        for (unsigned int l = 0; l < 2; ++l)
-          w(l, j) += A(j, i) * h(l, i);
+    if (all_features) {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        for (int j = 0; j < w.cols(); ++j)
+          for (unsigned int l = 0; l < 2; ++l)
+            w(l, j) += A(j, samples[i]) * h(l, i);
+      }
+    } else {
+      for (unsigned int i = 0; i < h.cols(); ++i) {
+        for (int j = 0; j < w.cols(); ++j)
+          for (unsigned int l = 0; l < 2; ++l)
+            w(l, j) += A(features[j], samples[i]) * h(l, i);
+      }
     }
     nnls2InPlace(a, denom, w, nonneg);
   } else {
@@ -290,9 +433,15 @@ void projectInPlace(const Eigen::MatrixXd& A, const Eigen::MatrixXd& h, Eigen::M
     #pragma omp parallel for num_threads(threads) schedule(dynamic)
     #endif
     for (unsigned int i = 0; i < h.cols(); ++i) {
-      for (int j = 0; j < A.rows(); ++j)
-        for (unsigned int l = 0; l < k; ++l)
-          w(l, j) += A(j, i) * h(l, i);
+      if (all_features) {
+        for (int j = 0; j < w.cols(); ++j)
+          for (unsigned int l = 0; l < k; ++l)
+            w(l, j) += A(j, samples[i]) * h(l, i);
+      } else {
+        for (int j = 0; j < w.cols(); ++j)
+          for (unsigned int l = 0; l < k; ++l)
+            w(l, j) += A(features[j], samples[i]) * h(l, i);
+      }
     }
     if (L1 != 0) w.array() -= L1;
     #ifdef _OPENMP
