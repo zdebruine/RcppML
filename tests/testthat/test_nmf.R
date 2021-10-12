224a169f3991699f4989f7203f7cd8777d185869
zdebruine <zacharydebruine@gmail.com>
2021-10-12 12:33:45 -0400

    update tests for S3 classes and function structures

--- tests/testthat/test_nmf.R
+++ tests/testthat/test_nmf.R
@@ -1,17 +1,18 @@
-testConvergence <- function(A, k, ...){
-  m0 <- nmf(A, k, v = F, tol = 1e-10, maxit = 1, ...)
-  m1 <- nmf(A, k, v = F, tol = 1e-10, maxit = 10, ...)
+testConvergence <- function(A, k, mask_zeros = FALSE, nonneg = TRUE, ...){
+  m0 <- nmf(A, k, v = F, tol = 1e-10, maxit = 1, mask_zeros = mask_zeros, ...)
+  m1 <- nmf(A, k, v = F, tol = 1e-10, maxit = 10, mask_zeros = mask_zeros, ...)
   # test convergence
-  expect_gt(mse(A, m0$w, m0$d, m0$h, ...), mse(A, m1$w, m1$d, m1$h, ...))
+  expect_gt(mse(m0, A, mask_zeros), mse(m1, A, mask_zeros))
   
   # test that non-negativity constraints are enforced
   p <- list(...)
   if(k > 1) { # k == 1 of non-negative inputs gives non-negative results regardless of constraints
-    is_nonneg <- is.null(p$nonneg)
-    expect_equal(min(m0$w) >= 0, is_nonneg)
-    expect_equal(min(m0$h) >= 0, is_nonneg)
-    expect_equal(min(m1$w) >= 0, is_nonneg)
-    expect_equal(min(m1$h) >= 0, is_nonneg)
+    if(nonneg){
+      expect_equal(min(m0$w) >= 0, TRUE)
+      expect_equal(min(m0$h) >= 0, TRUE)
+      expect_equal(min(m1$w) >= 0, TRUE)
+      expect_equal(min(m1$h) >= 0, TRUE)
+    }
   }
 }
 
@@ -28,10 +29,10 @@ does_nmf_converge <- function(A, k){
     testConvergence(A, k, update_in_place = T, nonneg = F, diag = F)
 }
 
-set.seed(123)
 library(Matrix)
 setRcppMLthreads(1)
-A <- abs(Matrix::rsparsematrix(100, 100, 0.1))
+A <- simulateNMF(nrow = 100, ncol = 100, k = 5, noise = 0.5, dropout = 0.5, seed = 123)
+A <- as(A, "dgCMatrix")
 test_that("nmf converges over several iterations for sparse asymmetric inputs (k = 1)", { does_nmf_converge(A, 1) })
 test_that("nmf converges over several iterations for sparse asymmetric inputs (k = 2)", { does_nmf_converge(A, 2)})
 test_that("nmf converges over several iterations for sparse asymmetric inputs (k = 5)", { does_nmf_converge(A, 5)})
@@ -76,34 +77,12 @@ set.seed(123)
 w_ <- matrix(runif(500), 5, 100)
 test_that("setting the random seed gives identical models (sparse)", {
   expect_equal(nmf(A, 5, maxit = 3, seed = 123, v = F)$w, nmf(A, 5, maxit = 3, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, maxit = 3, w_init = w_, v = F)$w, nmf(A, 5, maxit = 3, w_init = w_, v = F)$w)
-  expect_equal(nmf(A, 5, maxit = 3, seed = 123, v = F)$w, nmf(A, 5, maxit = 3, w_init = w_, v = F)$w)
+  expect_equal(nmf(A, 5, maxit = 3, seed = list(w_), v = F)$w, nmf(A, 5, maxit = 3, seed = w_, v = F)$w)
   expect_equal(all(nmf(A, 5, maxit = 3, seed = 123, v = F)$w == nmf(A, 5, maxit = 3, seed = 234, v = F)$w), FALSE)
 })
 A <- as.matrix(A)
 test_that("setting the random seed gives identical models (dense)", {
   expect_equal(nmf(A, 5, maxit = 3, seed = 123, v = F)$w, nmf(A, 5, maxit = 3, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, maxit = 3, w_init = w_, v = F)$w, nmf(A, 5, maxit = 3, w_init = w_, v = F)$w)
-  expect_equal(nmf(A, 5, maxit = 3, seed = 123, v = F)$w, nmf(A, 5, maxit = 3, w_init = w_, v = F)$w)
+  expect_equal(nmf(A, 5, maxit = 3, seed = w_, v = F)$w, nmf(A, 5, maxit = 3, seed = w_, v = F)$w)
   expect_equal(all(nmf(A, 5, maxit = 3, seed = 123, v = F)$w == nmf(A, 5, maxit = 3, seed = 234, v = F)$w), FALSE)
-})
-
-test_that("factorizing for specific samples or features acts as expected (sparse)", {
-  s <- sample(1:100, 20)
-  f <- sample(1:100, 30)
-  expect_equal(nmf(A, 5, samples = s, seed = 123, v = F)$w, nmf(A[, sort(s)], 5, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, samples = s, seed = 123, v = F, update_in_place = TRUE)$w, nmf(A, 5, samples = s, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, features = s, seed = 123, v = F, update_in_place = TRUE)$w, nmf(A[sort(s), ], 5, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, features = s, seed = 123, v = F)$w, nmf(A[sort(s), ], 5, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, features = f, samples = s, seed = 123, v = F)$w, nmf(A[sort(f), sort(s)], 5, seed = 123, v = F)$w)
-})
-A <- as.matrix(A)
-test_that("factorizing for specific samples or features acts as expected (dense)", {
-  s <- sample(1:100, 20)
-  f <- sample(1:100, 30)
-  expect_equal(nmf(A, 5, samples = s, seed = 123, v = F)$w, nmf(A[, sort(s)], 5, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, samples = s, seed = 123, v = F, update_in_place = TRUE)$w, nmf(A, 5, samples = s, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, features = s, seed = 123, v = F, update_in_place = TRUE)$w, nmf(A[sort(s), ], 5, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, features = s, seed = 123, v = F)$w, nmf(A[sort(s), ], 5, seed = 123, v = F)$w)
-  expect_equal(nmf(A, 5, features = f, samples = s, seed = 123, v = F)$w, nmf(A[sort(f), sort(s)], 5, seed = 123, v = F)$w)
 })
--- tests/testthat/test_project.R
+++ tests/testthat/test_project.R
@@ -9,18 +9,18 @@ test_that("Testing RcppML::project", {
   
   x <- as(x, "dgCMatrix")
   # expect that two projections of y give approximately equal results (SPARSE)
-  expect_equal(project(x, y), project(x, y))
-  expect_equal(project(x, y, nonneg = FALSE), project(x, y, nonneg = FALSE))
+  expect_equal(project(y, x), project(y, x))
+  expect_equal(project(y, x, nonneg = FALSE), project(y, x, nonneg = FALSE))
   
   # project and test that error of solved model is better than error of a random model (SPARSE)
-  beta_hat <- project(x, y)
+  beta_hat <- project(y, x)
   beta_rand <- beta_hat[sample(nrow(beta_hat)), sample(ncol(beta_hat))]
   err_random_model <- sum((as.matrix(x) - y %*% beta_rand)^2)
   err_projected_model <- sum((as.matrix(x) - y %*% beta_hat)^2)
   expect_lt(err_projected_model, err_random_model)
 
   # test that nonneg successfully enforces non-negativity constraints (SPARSE)
-  beta_hat <- project(x, y)
+  beta_hat <- project(y, x)
   expect_equal(sum(beta_hat[beta_hat < 0]), 0)
   
 })
