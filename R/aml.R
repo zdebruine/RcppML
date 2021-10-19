917367bd03199eee8d60ea8a1b0141d10f11a41c
zdebruine <zacharydebruine@gmail.com>
2021-10-19 19:35:46 -0400

    seed patch for RcppML crossValidate method='impute'

--- R/aml.R
+++ R/aml.R
@@ -3,7 +3,7 @@
 #' @description ~800 differentially methylated regions (DMRs) in 123 Acute Myelogenous Leukemia (AML) samples, and 5 samples for putative cells of origin (GMP, LMPP, MEP) giving healthy DMR signatures.
 #'
 #' @details
-#' AML methylation signatures differ from their cell of origin by additive methylation and subtractive methylation, in addition to tumor-specific heterogeneity. These AML tumors originated from one of three healthy cell types (GMP, LMPP, or MEP), the challenge is to classify the cell of origin based on these healthy cell type DMRs.
+#' AML methylation signatures differ from their cell of origin by additive methylation and subtractive methylation, in addition to tumor-specific heterogeneity. These AML tumors originated from one of three healthy cell types (GMP, LMPP, or MEP), the challenge is to classify the cell of origin based on these healthy cell type DMRs. The true answer is unknown.
 #'
 #' @md
 #' @docType data
