6eb96ddbe5480b58e023180d3e7e78a48ee5784c
zdebruine <zacharydebruine@gmail.com>
2021-10-31 15:05:43 -0400

    update

--- R/simulateNMF.R
+++ R/simulateNMF.R
@@ -10,45 +10,44 @@
 #' @param seed seed for random number generation
 #' @export
 #' @importFrom stats cor rmultinom rnorm runif
-#' @return dense matrix
+#' @return list of dense matrix \code{A} and true \code{w} and \code{h} models
 #'
-simulateNMF <- function(nrow, ncol, k, noise = 0.5, dropout = 0.5, seed = NULL){
-  
-  if(!is.null(seed)) set.seed(seed)
+simulateNMF <- function(nrow, ncol, k, noise = 0.5, dropout = 0.5, seed = NULL) {
 
-  g <- rmultinom(1, ncol, rep(1, k))
-  b <- rmultinom(1, nrow, rep(1, k))
+  if (!is.null(seed)) set.seed(seed)
+  num_nzh <- round(rnorm(k, ncol / 2, sd = ncol / 4))
+  num_nzw <- round(rnorm(k, nrow / 2, sd = nrow / 4))
+  num_nzw[num_nzw < 2] <- 2
+  num_nzw[num_nzw > nrow] <- nrow
+  num_nzh[num_nzh < 2] <- 2
+  num_nzh[num_nzh > ncol] <- ncol
+
+  h <- matrix(0, k, ncol)
+  w <- matrix(0, nrow, k)
   
-  # generate H
-  H <- matrix(0, k, ncol)
-  tmp <- 0
-  for(i in 1:k){
-    H[i, (tmp+1):(tmp+g[i])] <- 1
-    tmp <- tmp+g[i]
-  } 	
-  
-  # generate W
-  W <- matrix(0, nrow, k)
-  tmp <- 0
-  for(i in 1:k){		
-    W[(tmp+1):(tmp+b[i]),i] <- abs(rnorm(b[i], 1, 1))
-    tmp <- tmp + b[i]
+  # decide which indices are going to be non-zero
+
+  for (i in 1:k){
+    h[i, sample(1:ncol, num_nzh[i])] <- abs(rnorm(num_nzh[i], 1, 1))
+    w[sample(1:nrow, num_nzw[i]), i] <- abs(rnorm(num_nzw[i], 1, 1))
   }
-  
+  w <- as.matrix(w %*% Diagonal(x = 1 / colSums(w)))
+  h <- as.matrix(Diagonal(x = 1 / rowSums(h)) %*% h)
+
   # build the input matrix
-  res <- W %*% H
+  res <- w %*% h
 
   # add noise
-  if(noise > 0){
+  if (noise > 0) {
     res <- res + matrix(rnorm(nrow * ncol, mean = 0, sd = noise), nrow, ncol)
     res[res < 0] <- 0
   }
-  
+
   # introduce dropout
-  if(dropout > 0){
+  if (dropout > 0) {
     d <- as.matrix(as(Matrix::rsparsematrix(nrow, ncol, 1 - dropout, rand.x = NULL), "dgCMatrix"))
     res <- res * d
   }
-  
-  res
+
+  list(A = res, w = W, h = H)
 }
