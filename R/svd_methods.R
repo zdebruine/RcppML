b753f94cdfaa32c9c7f0ae6d848786fef81a1044
Wyatt Barber <wyattjbarber@gmail.com>
2023-02-26 20:50:23 -0500

    Adjust test case

--- R/svd_methods.R
+++ R/svd_methods.R
@@ -9,55 +9,12 @@ setClass("svd",
   })
 
 
-
-setMethod("evaluate", signature = "svd", function(x, data, mask = NULL, missing_only = FALSE, ...) {
+setMethod("$", signature = "svd", function(x, name) {
   validObject(x)
-
-  if (missing_only && is.null(mask)) stop("a mask matrix must be specified to set 'missing_only = TRUE'")
-
-  # get 'data' in either sparse or dense matrix format and look for NA's
-  if (is(data, "sparseMatrix")) {
-    if (class(data)[[1]] != "dgCMatrix") data <- as(data, "dgCMatrix")
-    if (any(is.na(data@x))) {
-      if (!is.null(mask) && mask != "NA") stop("data contains 'NA' values. Either remove these values or specify \"mask = 'NA'\"")
-      mask <- is.na(data)
-    }
-  } else if (canCoerce(data, "matrix")) {
-    data <- as.matrix(data)
-    if (!is.numeric(data)) data <- as.numeric(data)
-    if (any(is.na(data))) {
-      if (!is.null(mask) && mask != "NA") stop("data contains 'NA' values. Either remove these values or specify \"mask = 'NA'\"")
-      mask <- is.na(as(data, "dgCMatrix"))
-    }
-  } else stop("'data' was not coercible to a matrix")
-
-  if (is.null(mask)) {
-    mask_matrix <- new("dgCMatrix")
-    mask_zeros <- FALSE
-  } else if (class(mask)[[1]] == "character" && mask == "zeros") {
-    mask_matrix <- new("dgCMatrix")
-    mask_zeros <- TRUE
-  } else {
-    mask_zeros <- FALSE
-    if (!canCoerce(mask, "dgCMatrix")) {
-      if (canCoerce(mask, "matrix")) {
-        mask <- as.matrix(matrix)
-      } else stop("could not coerce the value of 'mask' to a sparse pattern matrix (dgCMatrix)")
-    }
-    mask_matrix <- as(mask, "dgCMatrix")
-  }
-
-  if (class(data)[[1]] == "dgCMatrix") {
-    if (missing_only) {
-      Rcpp_mse_missing_sparse(data, mask_matrix, t(x@w), x@d, x@h, getOption("RcppML.threads"))
-    } else {
-      Rcpp_mse_sparse(data, mask_matrix, t(x@u), x@h, getOption("RcppML.threads"), mask_zeros)
-    }
-  } else {
-    if (missing_only) {
-      Rcpp_mse_missing_dense(data, mask_matrix, t(x@w), x@d, x@h, getOption("RcppML.threads"))
-    } else {
-      Rcpp_mse_dense(data, mask_matrix, t(x@w), x@d, x@h, getOption("RcppML.threads"), mask_zeros)
-    }
-  }
+  name <- tolower(name)
+  if (!(name %in% c("u", "v", "misc"))) {
+    if (!(name %in% names(x@misc))) {
+      stop("'name' not a slot in 'object' or component in 'object@misc'")
+    } else x@misc[[name]]
+  } else slot(x, name)
 })
--- tests/testthat/test_svd.R
+++ tests/testthat/test_svd.R
@@ -2,11 +2,11 @@ options(RcppML.threads = 1)
 options(RcppML.verbose = TRUE)
 
 testConvergence <- function(A, k, mask = NULL, ...){
-  m0 <- svd(A, k)
-  # test convergence
-  dbg <- m0$dbg
-  
-  expect_gt(dbg[0], dbg[1])
+  m <- svd(A, k)
+ 
+  # Test that reconstructed array is equal to input
+  A_reconstruct = m$u %*% m$v
+  expect_equal(A_reconstruct, A)
 }
 
 does_svd_converge <- function(A, k){
@@ -17,9 +17,11 @@ does_svd_converge <- function(A, k){
     testConvergence(A, k)
 }
 
+
 library(Matrix)
-A <- simulateNMF(nrow = 50, ncol = 50, k = 5, noise = 0.5, dropout = 0.5, seed = 123)
-A <- as(A$A, "dgCMatrix")
+U_ <- matrix(rnorm(100), ncol=5)
+V_ <- matrix(rnorm(100), nrow=5)
+A <- as(U_ %*% V_, "dgCMatrix")
 #test_that("svd converges over several iterations for sparse asymmetric inputs (k = 5)", { does_svd_converge(A, 5)})
 A <- as.matrix(A)
 test_that("svd converges over several iterations for dense asymmetric inputs (k = 5)", { does_svd_converge(A, 5)})
